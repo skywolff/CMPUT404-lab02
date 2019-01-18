8dabd10bf9792ec17ba37d7d0bc81fcde5ff5193
Alex Li <tianyi7@ualberta.ca>
2019-01-17 17:31:23 -0700

    finished get google.com request

--- client.py
+++ client.py
@@ -1,13 +1,39 @@
 #!/bin/python3
-
 import socket
 
 HOST = 'www.google.com'
 PORT = 80
-request = 'GET / HTTP/1.1\r\n\r\n'
-
+request = 'GET / HTTP/1.0\r\nHost: {}\r\n\r\n'.format(HOST)
+BUF_SIZE = 1024
 
 def main():
+    addrInfo = socket.getaddrinfo(HOST, PORT)
+    for addr in addrInfo:
+        (family, sockType, proto, canonname, sockaddr) = addr
+        if family == socket.AF_INET and sockType == socket.SOCK_STREAM:
+            print(addr)
+            get_request(addr)
+
+def get_request(addr):
+    (family, sockType, proto, canonname, sockaddr) = addr
+    try: 
+        s = socket.socket(family, sockType, proto)
+        s.connect((HOST,PORT))
+        s.sendall(request.encode())
+        s.shutdown(socket.SHUT_WR)
+        full_data = b""
+        while True:
+            data = s.recv(BUF_SIZE)
+            if not data: break
+            full_data += data
+        print(full_data)
+    except Exception as e:
+        print(e)
+    finally:
+        s.close()
+
+
+def main2():
     print(request)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
@@ -20,6 +46,7 @@ def main():
                 # print(response.decode('utf-8'))
             except socket.timeout:
                 return
+
 
 
 if __name__ == "__main__":
